7926e2e0d90cef7fd01b193f4b71be075a87736e
Lucas Baccin <lbaccin1998@gmail.com>
2026-08-07 01:58:01 -0300

    CI Workflow para o projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require('allure-cypress/reporter');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config);
       
+      return config;
     },
     baseUrl: process.env.BASE_URL || 'http://localhost:3000' //Deve ser feita desse maneira, pois é um arquivo de configuração, tendo prioridade ao cypress.env; || delimita um OU, já que se o primeiro metódo não passar, o segundo é executado
   },
